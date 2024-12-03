af49b2efe3515c77e894e77647f0d179f352ebc9
franzfelini <arnautovic.feda@gmail.com>
2024-12-03 19:45:01 +0100

    before the start

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [InputValue, setInputValue] = useState("");
   const [country, setCountry] = useState("");
   const [region, setRegion] = useState("");
+  const [timezone, setTimezone] = useState("");
 
   useEffect(() => {
     getData();
@@ -17,9 +18,11 @@ function App() {
       .then((response) => {
         const country = response.data.country;
         const region = response.data.region;
+        const timezone = response.data.timezone;
 
         setCountry(country);
         setRegion(region);
+        setTimezone(timezone);
       })
       .catch((error) => {
         console.error(error);
@@ -35,37 +38,35 @@ function App() {
   return (
     <>
       <div className="flex justify-center">
-        <div className="flex justify-center mt-[10em] p-[5em] rounded-3xl bg-slate-700 w-[40em]">
+        <div className="flex justify-center mt-[10em] p-[5em] rounded-3xl bg-slate-850 w-[40em]">
           <div className="flex flex-col mt-[2em]flex-col gap-[2em] ">
-            <h1 className="font-normal justify-center text-white w-[10em] ml-[3em] text-3xl font-[Tahoma]">
-              IP ADRESS TRACKER
+            <h1 className="font-normal justify-center text-white w-[10em] text-3xl font-[Tahoma]">
+              IP TRACKER
             </h1>
 
             <input
-              className="flex w-[30em] h-[3.5em] justify-center p-[1em]"
+              className="flex w-[30em] h-[3.5em] justify-center p-[1em] rounded-full bg-black border-[1px] text-white"
               type="text"
               value={InputValue}
               onChange={handleInputChange}
-              placeholder="Enter IP address"
+              placeholder="Enter an IP address"
             />
             <div className="flex justify-center">
               <button
-                className="flex w-[20em] h-[3.5em] justify-center items-center font-extralight border-[0.2px] border-white mt-[1em] bg-slate-900 text-white rounded-sm"
+                className="flex w-[20em] h-[3.5em] justify-center items-center font-extralight mt-[1em] bg-sky-800 text-white rounded-sm"
                 onClick={getData}
               >
-                GET DATA
+                GET USER DATA
               </button>
             </div>
-            <div className="bg-white w-[30em] h-[4em] rounded-sm font-extralight text-black justify-center p-[1em]">
+            <div className="bg-black w-[30em] h-[10em] rounded-fu3xl font-extralight text-white border-[1px] border-white justify-center p-[1em]">
               {country && region ? (
                 <p className="flex justify-center">
-                  Country: {country}, Region: {region}
+                  Country: {country} <br></br> Region: {region} <br></br>
+                  {timezone}
                 </p>
               ) : (
-                <p>
-                  Enter an IP address and click "GET DATA" to see the country
-                  and region.
-                </p>
+                <p>Enter an IP address</p>
               )}
             </div>
           </div>
